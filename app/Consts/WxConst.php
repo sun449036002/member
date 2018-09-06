24f0e06abc0beb0e3cc9f1e0f1ce225932c63dc7
Administrator <449036002@qq.com>
2018-09-06 09:21:22 +0800

    'mtid'

--- app/Consts/WxConst.php
+++ app/Consts/WxConst.php
@@ -12,6 +12,7 @@ namespace App\Consts;
 class WxConst
 {
     //提现申请审核通知
-    const TEMPLATE_ID_FOR_WITHDRAW_NOTICE = "jhtDEOEiby3y2WyxY7K74rWxHPyqCzhm3Mvrexhczf0";
+//    const TEMPLATE_ID_FOR_WITHDRAW_NOTICE = "jhtDEOEiby3y2WyxY7K74rWxHPyqCzhm3Mvrexhczf0";
+    const TEMPLATE_ID_FOR_WITHDRAW_NOTICE = "XM1EKXf6qr5KZTZQnFpZJ0XT7S6uHiekIKF_i4Mw0uQ";
 
 }
